13ae3ea442632f8680da56be1f99082cf720d4a3
Shin Seonyeong <83331566+ShinSeonyeong@users.noreply.github.com>
2025-01-23 13:22:05 +0900

    Add files via upload

---
+++ 0123/ex06.js
@@ -0,0 +1,2 @@
+var 이름 = '신선영';
+console.log(이름);
---
+++ 0123/ex07.js
@@ -0,0 +1,11 @@
+console.log(3)
+
+var x = 10;
+if (x > 1) {
+    console.log(x)
+    console.log("실행된다.");
+}
+
+for (var i = 0; i < 3; i++) { 
+    console.log(i); 
+}
