8af38f0cb666aba21fa605ac271fcb510f6e2f64
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-02-11 09:47:20 +0900

    no message

---
+++ 0210/ex06.js
@@ -0,0 +1,19 @@
+//for문: 0~5까지 출력
+//while문: 0~5까지 출력
+
+console.log("for 구문 실행");
+let i = 0;
+for ( ; i < 6; ) {
+    console.log(i);
+    i++;
+}
+console.log(`i=${i}`);
+
+
+console.log("while 구문 실행");
+var j = 0;
+while (j < 6) {
+    console.log(j);
+    j++;
+}
+console.log(`j=${j}`);
---
+++ 0210/ex10.js
@@ -0,0 +1,10 @@
+var a = 20;
+
+console.log(`a=${a}`);
+while(a<10){
+    console.log(`a가 10보다 작은 동안 실행`);
+}
+
+// do {
+//     console.log(`a=${a}`);
+// } while (a < 10);
