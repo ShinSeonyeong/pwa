f5aa98213d73e8a5825e4616e462eee1c35781d7
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-02-21 14:29:20 +0900

    no message

---
+++ 0221/ex16.js
@@ -0,0 +1,8 @@
+function solution(numbers, num1, num2) {
+    var answer = []
+    for (var i = 0; i < numbers.length; i++){
+        console.log(numbers[i]);
+    }
+}
+
+solution([1, 2, 3, 4, 5], 1, 3)
