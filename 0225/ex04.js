137712b9c7f3e29205f120fccf7bf546aeb5d525
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-02-25 14:28:15 +0900

    no message

--- 0225/ex04.js
+++ 0225/ex04.js
@@ -1,8 +1,27 @@
+function deepFreeze(target) {
+
+    // target이 객체이고 freeze되지 않은 객체만 동결한다
+    if (target && target === 'object' && !Object.isFrozen(target)) {
+        Object.freeze(target);
+
+        Object.keys(target).forEach(key => {
+            deepFreeze(target[key]);
+        });
+    }
+    return target;
+}
+
 const person = {
     name: 'LEE',
     addr: { city: 'seoul' },
     friends: { name: 'Park', addr: { city: "busan" } }
 };
+
+deepFreeze(person);
+
+person.addr.city='busan';
+console.log(person);
+
 
 // 추가X, 삭제O, 프로퍼티 값변경 O
 // Object.preventExtensions(person);
@@ -11,12 +30,12 @@ const person = {
 // Object.seal(person);
 
 // 추가X, 삭제X, 프로퍼티 값변경 X, configuable: false, writable: false
-Object.freeze(person);
-Object.freeze(person.addr);
-Object.freeze(person.friends.addr);
+// Object.freeze(person);
+// Object.freeze(person.addr);
+// Object.freeze(person.friends.addr);
 
-person.addr.city = 'jeju';
-console.log(person);
+// person.addr.city = 'jeju';
+// console.log(person);
 
 // person.age = 20;
 // console.log(person);
