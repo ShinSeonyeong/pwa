b14113918c4ba7558552e24a961abc52f277d1c6
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-03-28 15:07:27 +0900

    no message

---
+++ react_study/ex0328/src/Users.js
@@ -0,0 +1,12 @@
+export const a = 10;
+
+export class Users {
+    constructor() {
+        this.users = [
+            {id: 1, name: 'aaa', email: 'aaa@example.com'},
+            {id: 2, name: 'bbb', email: 'bbb@example.com'},
+            {id: 3, name: 'bbc', email: 'bbc@example.com'},
+            {id: 4, name: 'bbc', email: 'bbc@example.com'},
+        ]
+    }
+}
