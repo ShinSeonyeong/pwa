2062565e57cb8b824f5114611bad1ba74e2e581d
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-04-07 17:31:01 +0900

    no message

--- react_study/ex0407/src/pages/Lotto.jsx
+++ react_study/ex0407/src/pages/Lotto.jsx
@@ -1,11 +1,16 @@
 import React, {useEffect, useRef, useState} from 'react';
 
 function Lotto(props) {
+    const [firstWinamnt, setfirstWinamnt] = useState(0);
+    const [firstAccumamnt, setfirstAccumamnt] = useState(0);
+    const [drwNoDate, setDrwNoDate] = useState(0);
+    const [bnusNo, setBnusNo] = useState(0);
     const [drwNo, setDrwNo] = useState(0);
     const [numbers, setNumbers] = React.useState([]);
     const [myNumbers, setMyNumbers] = React.useState([]);
     const inputRef = useRef(null);
 
+    // firstAccumamnt(총금액), firstWinamnt(1인수령금액)
     async function getMytNumbers() {
         // const arr = [...new Array(6)].map(
         //     (_, i) => Math.trunc(Math.random() * 46) + 1); // 1개의 랜덤숫자 출력됨
@@ -30,6 +35,10 @@ function Lotto(props) {
 
         setNumbers(([res.drwtNo1, res.drwtNo2, res.drwtNo3, res.drwtNo4, res.drwtNo5, res.drwtNo6])); // .then(json => setNumbers([res.drwtNo1, res.drwtNo2, res.drwtNo3, res.drwtNo4, res.drwtNo5, res.drwtNo6]))
         setDrwNo(res.drwNo);
+        setBnusNo(res.bnusNo);
+        setDrwNoDate(res.drwNoDate);
+        setfirstAccumamnt(res.firstAccumamnt);
+        setfirstWinamnt(res.firstWinamnt);
     }
 
     useEffect(() => {
@@ -39,7 +48,11 @@ function Lotto(props) {
         <div>
             <h1>Lotto</h1>
             <h1>{drwNo}회</h1>
-            <input type="text" ref={inputRef}/>
+            <h5>보너스번호: {bnusNo}</h5>
+            <h5>로또 추첨 날짜: {drwNoDate}</h5>
+            <h5>총금액: {firstAccumamnt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</h5>
+            <h5>1인수령금액: {firstWinamnt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</h5>
+            로또 회차번호: <input type="text" ref={inputRef}/>
             <button onClick={getNumbers}>조회</button>
             <div style={{display: 'flex', gap: '1rem'}}>
                 {numbers.map(number => (
