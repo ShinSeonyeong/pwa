073681c2e3ba8058405f2de0d50e34d2b29dcd6b
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-04-09 12:12:31 +0900

    no message

--- javascript_study/0409/ex09.js
+++ javascript_study/0409/ex09.js
@@ -1,21 +1,29 @@
 // 피보나치 수열
-const fibo = {
-  [Symbol.iterator]() {
-    // const [myNum, setMyNum] = [10, function () {}];
-    let [pre, cur] = [0, 1];
-    return {
-      next() {
-        // value값: 1,2,3,5,8
-        [pre, cur] = [cur, pre + cur];
-        return { value: cur, done: false };
-      },
-    };
-  },
+const fibo = function (max) {
+  // [Symbol.iterator](max): max값을 이렇게 넣으면 Symbol.iterator 규약이 깨짐
+  return {
+    [Symbol.iterator]() {
+      // const [myNum, setMyNum] = [10, function () {}];
+      let [pre, cur] = [0, 1];
+      const max = 30;
+      return {
+        next() {
+          // value값: 1,2,3,5,8
+          [pre, cur] = [cur, pre + cur];
+          return { value: cur, done: cur > max };
+        },
+      };
+    },
+  };
 };
 
-const iter = fibo[Symbol.iterator]();
-console.log(iter.next());
-console.log(iter.next());
-console.log(iter.next());
-console.log(iter.next());
-console.log(iter.next());
+for (const element of fibo(30)) {
+  console.log(element); // done: false이면 무한루프, 조건 걸어야 함.
+}
+
+// const iter = fibo[Symbol.iterator]();
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
