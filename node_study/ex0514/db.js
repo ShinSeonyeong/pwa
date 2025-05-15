daf704a9f1fc30dc23768d2335c7c50d81ae4804
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-05-15 11:13:19 +0900

    no message

---
+++ node_study/ex0514/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+// console.log(process.env.DB_HOST);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+// console.log(process.env.DB_NAME);
+
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST, // mysql 주소
+    user: process.env.DB_USER, // mysql 사용자
+    password: process.env.DB_PASSWORD, // mysql 비밀번호
+    database: process.env.DB_NAME, // mysql 데이터베이스
+    waitForConnections: true, // 연결 기다림, 대기여부
+    connectionLimit: 10, // 최대연결 수
+    queueLimit: 0 // 대기열 최대 수 작업지시 개수 (무한대까지 연결할 수 있도록 0으로 설정(기본값 0))
+});
+
+module.exports = pool;
+
+module.exports = {
+    aa: 'aa',
+}
--- node_study/ex0514/index.js
+++ node_study/ex0514/index.js
@@ -1,44 +1,53 @@
-const http = require('http'); // 웹서버 역할을 해주는 라이브러리
-const fs = require('fs').promises;
+const pool = require('./db');
+console.log(pool);
 
-http.createServer(async (req, res) => {
+const http = require("http"); // 웹서버 역할을 해주는 라이브러리
+const fs = require("fs").promises;
+
+http
+  .createServer(async (req, res) => {
     console.log(req.url);
     try {
-        if (req.url === '/') {
-            const password = '비밀번호'; // 숨겨진 데이터
-            const indexhtml = await fs.readFile('./index.html', 'utf-8'); // index.html 파일을 읽어오는 것
-            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'}); // 문자열이 날아간다는 의미
-            return res.end(indexhtml); // fs 라이브러리 사용하기 전 코드: res.end('<html><body><h1>Hi</h1></body></html>\n');
-        } else if (req.url === '/join' && req.method === 'POST') {
-            // 한글이 들어오는 것 맞추기 위해 utf-8로 인코딩
-            req.setEncoding('utf8');
+      if (req.url === "/") {
+        const password = "비밀번호"; // 숨겨진 데이터
+        const indexhtml = await fs.readFile("./index.html", "utf-8"); // index.html 파일을 읽어오는 것
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" }); // 문자열이 날아간다는 의미
+        return res.end(indexhtml); // fs 라이브러리 사용하기 전 코드: res.end('<html><body><h1>Hi</h1></body></html>\n');
+      } else if (req.url === "/join" && req.method === "POST") {
+        // 한글이 들어오는 것 맞추기 위해 utf-8로 인코딩
+        req.setEncoding("utf8");
 
-            let body = ''; // body 문자열 선언
-            // request 요청한 클라이언트 data로 들어오는 것이 있다면
-            // data모아서 body에 넣어준다.
-            req.on('data', (data) => {
-                body += data;
-            });
-            req.on('end', () => {
-                console.log(body); // body 내용 출력
-                const {id, password} = JSON.parse(body); // body를 JSON.parse 객체로 변환
-                console.log(id, password);
-            })
+        let body = ""; // body 문자열 선언
+        // request 요청한 클라이언트 data로 들어오는 것이 있다면
+        // data모아서 body에 넣어준다.
+        req.on("data", (data) => {
+          body += data;
+        });
+        req.on("end", () => {
+          console.log(body); // body 내용 출력
+          const { id, password } = JSON.parse(body); // body를 JSON.parse 객체로 변환
+          console.log(id, password);
 
-            const obj = {name: "홍길동", age: 20}
-            res.writeHead(201, {'Content-Type': 'application/json; charset=utf-8'});
-            return res.end(JSON.stringify(obj));
-        } else if (req.url === '/login') {
-            res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-            return res.end('로그인 성공');
-        }
-        res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
-        return res.end('잘못된 경로입니다.')
+          //   mysql에 저장하는 코드만들예정
+        });
+
+        const obj = { name: "홍길동", age: 20 };
+        res.writeHead(201, {
+          "Content-Type": "application/json; charset=utf-8",
+        });
+        return res.end(JSON.stringify(obj));
+      } else if (req.url === "/login") {
+        res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+        return res.end("로그인 성공");
+      }
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      return res.end("잘못된 경로입니다.");
     } catch (e) {
-        console.log(e);
-        res.writeHead(500, {'Content-Type': 'text/html; charset=utf-8'});
-        res.end(e.message);
+      console.log(e);
+      res.writeHead(500, { "Content-Type": "text/html; charset=utf-8" });
+      res.end(e.message);
     }
-}).listen(8080, '0.0.0.0', () => {
-    console.log('Server started on port 8080');
-})
+  })
+  .listen(8080, "0.0.0.0", () => {
+    console.log("Server started on port 8080");
+  });
