5afd1eb10034bac5026165edb770eabe6c26dba3
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-05-27 10:36:07 +0900

    no message

--- react_study/front02/src/App.jsx
+++ react_study/front02/src/App.jsx
@@ -2,41 +2,55 @@ import './App.css'
 import axios from "axios";
 import {useEffect, useState} from "react";
 
-const API_URL = import.meta.env.VITE_API_NODE_URL
+const API_URL = import.meta.env.VITE_API_NODE_URL;
+
 
 function App() {
-  const [test, setTest] = useState("안녕 TEST");
+  const hello = "Hello World";
+  const [data, setData] = useState()
 
-  const getRoot = async () => {
-    // console.log("get Root");
-    // const result = await axios.get(API_URL);
+
+
+  const getRoot = async () =>{
+    // const result = await axios.get("http://localhost:8080");
+    // console.log(result);
     // console.log(result.data);
 
-    const {data, error} = await axios.get(`${API_URL}`);
-    console.log(data);
-    console.log(error);
-    setTest(data);
+    const {data,error} = await axios.get(`${API_URL}`);
+    // console.log(data);
+    // console.log(error);
+
+    setData(data)
+
   }
 
+  //  푸시 알람 받을
   useEffect(() => {
+    console.log("service worker");
     if ("serviceWorker" in navigator && "PushManager" in window) {
-      console.log("Service worker");
       navigator.serviceWorker.ready.then((registration) => {
         console.log("service worker ready");
         registration.pushManager
             .subscribe({
               userVisibleOnly: true,
-              // 공개키 설정
               applicationServerKey: "BMJypUqPyy71zfr0ErUBVAVce6JE1AMbZzXa45C5dKSjjmcYnrcN9gBYtJu9xmam9OmoS1wfa51tKLigNvzRsIk",
             })
             .then((subscription) => {
-              return fetch(`{API_URL}/subscribe`, {
+              return fetch(`${API_URL}/subscribe`, {
                 method: "POST",
                 body: JSON.stringify(subscription),
                 headers: {
                   "Content-Type": "application/json",
                 },
-              });
+              })
+                  .then((res)=>{
+                    console.log(res);
+                    console.log('구독성공');
+                  })
+                  .catch((err)=>{
+                    console.log("구독실패");
+                    console.log(err);
+                  });
             })
             .catch((error) => {
               console.error("푸시 구독 실패:", error);
@@ -47,13 +61,10 @@ function App() {
 
   return (
       <>
-        <h1>Hello React</h1>
-        <p>Hello React! + {test}</p>
-        <button onClick={() => {
-          getRoot();
-        }}>백앤드요청
-        </button>
+        <h1>{hello}</h1>
+        <p>{data}</p>
 
+        <button onClick={getRoot}>백엔드요청</button>
       </>
   )
 }
