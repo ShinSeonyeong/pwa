59d68f8f57eef6b869fc7b6b3b959f816c08b3e7
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-06-24 13:12:31 +0900

    no message

---
+++ cs_study/Chap02/Chap02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕");
+            Console.WriteLine(32.GetType());
+            Console.WriteLine(129L.GetType());
+
+            Console.WriteLine(3.14f.GetType());
+            Console.WriteLine(3.14.GetType());
+
+            Console.WriteLine(true.GetType());
+            Console.WriteLine("안녕".GetType());
+            Console.WriteLine("A".GetType());
+            Console.WriteLine("안녕".Length); // 문자열의 길이
+            Console.WriteLine("asdf".ToUpper()); // 문자열을 대문자로 변환
+            Console.WriteLine("asdsAAD".ToLower()); // 문자열을 소문자로 변환
+            Console.WriteLine("안녕".Substring(1)); // 문자열의 일부를 추출
+
+            int a = 10;
+            double b = 3.14;
+
+            string c = "안녕하세요";
+            bool d = true;
+            char e = 'a';
+            var f = 100;
+            Console.WriteLine(f.GetType());
+        }
+    }
+}
---
+++ cs_study/Chap02/win02/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text,"첫번째 수");
+            MessageBox.Show(textBox2.Text, "두번째 수");
+
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            int a = 10;
+            Console.WriteLine($"\"a={a}\"\teadsf\nasdf");
+            //MessageBox.Show($"\"{(num1+num2).ToString()}\"", "더한값");
+            MessageBox.Show($"\"{num1-num2}\"");
+            //MessageBox.Show((int.Parse(textBox1.Text) + int.Parse(textBox2.Text)).ToString(), "더한 값");
+        }
+    }
+}
