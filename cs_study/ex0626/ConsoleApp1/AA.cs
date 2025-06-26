4b3892c5ac7c7e7f7b4918d9e4795f8de81fabc7
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-06-26 12:12:25 +0900

    no message

---
+++ cs_study/ex0626/ConsoleApp1/AA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //namespace 안에 클래스들을 선언가능
+    //클래스 안에 함수들을 선언 가능
+    //AA 클래스 안에 doA 함수가 있음
+    public class AA
+    {
+        public void doA()
+        {
+            Console.WriteLine("AA doA 함수");
+        }
+        public static void doB()
+        {
+            Console.WriteLine("AA doB 함수");
+        }
+    }
+}
---
+++ cs_study/ex0626/winform/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform
+{
+    public partial class Form1 : Form
+    {
+        int num = 10; // 클래스가 만들어질 때 변수가 생성됨.
+        public Form1() //생성자
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num = num + 1;
+            num++;
+            Console.WriteLine(num);
+            Console.WriteLine($"textbox1.Text={textBox1.Text}");
+            //textBox1.Text가 비어있지 않으면 
+            if(textBox1.Text != "")
+            {
+                int textboxvalue = int.Parse(textBox1.Text);
+                textBox1.Text = ""; //textBox1의 값을 지움
+            }
+            else
+            {
+                label2.Text = num.ToString(); //textBox1의 값을 지움
+            }
+            label2.Text = num.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("출력");
+        }
+    }
+}
