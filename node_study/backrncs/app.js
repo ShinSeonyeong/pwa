8c7b88876785ae9849efe8bde26398413b72a6db
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-07-09 11:13:02 +0900

    no message

---
+++ node_study/backrncs/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var logger = require('morgan');
+
+require("dotenv").config();  // .env
+const cors = require('cors');
+
+var indexRouter = require('./routes/index');
