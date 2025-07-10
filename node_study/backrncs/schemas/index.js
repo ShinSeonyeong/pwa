c724e253cd3e8ff49c6fc623dc9ce7abeb547708
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-07-10 09:51:32 +0900

    no message

--- node_study/backrncs/schemas/index.js
+++ node_study/backrncs/schemas/index.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
-const {MONGO_PASSWORD} = process.env;
+const mongoose = require("mongoose");
+require("dotenv").config(); // process.env env에 설정한 내용을 가져옴
 
-const mongo_url = `mongodb+srv://tlstjsdud1920:${MONGO_PASSWORD}@cluster0.itrfgeo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const mongo_url = process.env.MONGO_URL;
 
 const connect = () => {
-  mongoose.connect(mongo_url, {dbName: 'rncs'})
-      .then(() => {
-        console.log("connect to mongo success");
-      })
-      .catch((err) => {
-        console.error("Mongo DB Error", err);
-      })
-}
+  mongoose
+    .connect(mongo_url, { dbName: "rncs" })
+    .then(() => {
+      console.log("connect to mongo success");
+    })
+    .catch((err) => {
+      console.error("Mongo DB Error", err);
+    });
+};
 
 module.exports = connect;
