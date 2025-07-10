a1815025b583f87448dc1b2c8d11ca7b1ab5d2ba
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-07-10 12:02:51 +0900

    no message

--- node_study/backrncs/routes/users.js
+++ node_study/backrncs/routes/users.js
@@ -14,12 +14,24 @@ router.post("/register", async (req, res, next) => {
 });
 
 router.get('/', async (req, res, next) => {
-  try{
+  try {
     const users = await User.find();
     return res.json(users);
-  }catch (err){
+  } catch (err) {
     return res.status(500).json(err);
   }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params;
+    // supabase에선 supabase.from('테이블명').delete().eq()로 달아줬지만 여기선 findByIdAndDelete 이런 함수 쓰면 okay다!!!
+    const deleted = await User.findByIdAndDelete(id);
+    return res.json({message: '삭제성공', deleted});
+  } catch (e) {
+    return res.status(500).json(e);
+  }
+
 })
 
 module.exports = router;
